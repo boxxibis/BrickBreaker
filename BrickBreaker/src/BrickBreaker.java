f2269586e083aae54fa9c95690a0d4bd3f1e50bf
epena6656 <epena6656@001LAB153010825.neisd.ad>
2016-05-12 10:10:00 -0500

    Outline

--- BrickBreaker/src/BrickBreaker.java
+++ BrickBreaker/src/BrickBreaker.java
@@ -1,16 +1,40 @@
+import java.awt.BorderLayout;
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class BrickBreaker {
 	JFrame frame;
+	DrawPanel drawPanel;
+	boolean up = false;
+	boolean down = false;
 
 	public static void main(String[] args) {
+		new BrickBreaker().makePanel();
+	}
+
+	public void makePanel() {
+		frame = new JFrame("Brick Breaker");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		drawPanel = new DrawPanel();
+		frame.getContentPane().add(BorderLayout.CENTER, drawPanel);
+		frame.setVisible(true);
+		frame.setResizable(false);
+		frame.setSize(800, 800);
+		frame.setLocation(375, 55);
+		startGame();
+	}
+
+	public void startGame() {
 
 	}
 
-}
+	class DrawPanel extends JPanel {
 
-class runBrick {
-	public void runBrick() {
+		public void paintComponent(Graphics g) {
 
+		}
 	}
+
 }
